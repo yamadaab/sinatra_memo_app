bd970152dc1ff72b6d2f2c7a71a1bd9efb9ec8f1
yamadaab <s.yamada.1125@gmail.com>
2020-08-27 13:50:54 +0900

    add views_new

--- app.rb
+++ app.rb
@@ -6,3 +6,14 @@ get '/' do
   @memos = Dir.glob("*", base: "memo")
   erb:index
 end
+
+get '/new' do
+  erb:new
+end
+
+post '/create' do
+  @title = params[:title]
+  @text = params[:text]
+  File.open("./memo/#{@title}", 'wb') { |f| f.print @text}
+  redirect "/"
+end
